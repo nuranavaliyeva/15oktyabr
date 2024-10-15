1bdc7cb5dac037673403138c59fd6b5e40349f3c
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-10-15 10:45:38 +0400

    first commit

---
+++ ConsoleApp11/Models/product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Models
+{
+    internal class product
+    {
+        public string Id;
+        public string BrandName;
+        public string Model;
+        public decimal Price;
+        public decimal Cost;
+        public decimal Income;
+        public int Count;
+        public string GetInfo()
+        {
+            return $"Id: {Id}, Brand: {BrandName}, Model: {Model}, Price: {Price}, Cost: {Cost}, Income: {Income}, Count: {Count}";
+        }
+        public product(string brandName, string model, decimal price, decimal cost, int count)
+        {
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            Count = count;
+            Income = 0;
+
+            Id = $"{BrandName.Substring(0, 2)}{Model.Substring(0, 2)}";
+            
+
+        }
+        public void Sale(int num)
+        {
+            if (num > 0)
+            {
+                decimal totalPrice = Price * num;
+                decimal totalCost = Cost * num;
+                Income += (totalPrice - totalCost);
+
+                Count -= num;
+            }
+
+            else
+            {
+                Console.WriteLine("Yeterli mehsul yoxdur.");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp11.Models;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            product product = new product("Apple", "Iphone13", 10m, 20m, 11);
+            product.Sale(-1);
+            Console.WriteLine(product.GetInfo());
+            
+
+
+        }
+    }
+}
